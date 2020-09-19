b619be02a4f796875550126fbedfd5b415ea1957
suzuki-321 <y0u7t0a9@gmail.com>
2020-09-19 12:34:20 +0900

    Second commit

--- display.php
+++ display.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-
 <meta charset="utf-8">
 <title>数字</title>
 </head>
 <body>
 <h1>1から50</h1>
 <?php
-for ($a = 1; $a <= 50; $a++) {
+$a = 1;
+while ($a <= 50) {
 	echo $a . '<br>';
+	$a++;
 }
 ?>
 </body>
